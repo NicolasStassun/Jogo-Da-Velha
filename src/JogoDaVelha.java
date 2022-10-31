8007e6675a692881dc9a0d9f0c4a4581e4ada3c1
NicolasStassun <nicolas_stassun@estudante.sesisenai.org.br>
2022-10-31 16:13:58 -0300

    2.0

--- src/JogoDaVelha.java
+++ src/JogoDaVelha.java
@@ -8,6 +8,7 @@ public class JogoDaVelha {
 
         Random ppt = new Random();
         int jogadorOpcao, pcOpcao;
+        String pptPc="", pptJogador="";
 
         System.out.println("Escreva se você deseja: \n" +
                 "0 - Pedra.\n" +
@@ -16,15 +17,45 @@ public class JogoDaVelha {
         jogadorOpcao = sc.nextInt();
         pcOpcao = ppt.nextInt(2);
 
+        if(jogadorOpcao!=0 & jogadorOpcao!=1 & jogadorOpcao!=2){
+            System.out.printf("Opcao inválida.");
+            main(args);
+        }
+
+        if(pcOpcao==0){
+            pptPc="Pedra";
+        }
+        else if(pcOpcao==1){
+            pptPc="Papel";
+        }
+        else if(pcOpcao==2){
+            pptPc="Tesoura";
+        }
+        if(jogadorOpcao==0){
+            pptJogador="Pedra";
+        }
+        else if(jogadorOpcao==1){
+            pptJogador="Papel";
+        }
+        else if(jogadorOpcao==2){
+            pptJogador="Tesoura";
+        }
+
+
+
         if(jogadorOpcao==pcOpcao){
+            System.out.printf(pptJogador+" x "+pptPc+"\n");
             System.out.println("O Jogo empatou.");
             main(args);
         }
         else if((jogadorOpcao==1 & pcOpcao==2) || (jogadorOpcao==2 & pcOpcao==0)||(jogadorOpcao==0 & pcOpcao==1)){
+
+            System.out.printf(pptJogador+" x "+pptPc+"\n");
             System.out.println("O Jogador Perdeu!");
             main(args);
         }
         else{
+            System.out.printf(pptJogador+" x "+pptPc+"\n");
             System.out.println("O Jogador Ganhou!");
             main(args);
         }
